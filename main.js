d3d3cddfb3f7a1321b06af2ddd51cc07d9b35fbc
Scott Fennie <s.fennie7@gmail.com>
2021-08-28 18:42:59 -0600

    checklist met

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let testosterone = 0
+let tsec = 0
+let multiply = 0
 
 let clickUpgrades = {
     weights: {
@@ -18,6 +20,11 @@ let autoUpgrades = {
         price: 30,
         quantity: 0,
         multiplier: 20,
+    },
+    codelessons: {
+        price: 100,
+        quantity: 0,
+        multiplier: 50,
     }
 }
 
@@ -45,8 +52,26 @@ function addAuto() {
     }
 }
 
+function Multiplier() {
+
+    for (let key in autoUpgrades) {
+        let item = autoUpgrades[key]
+
+        multiply += (item.quantity * item.multiplier)
+    }
+}
 
 
+function Tsec() {
+
+    for (let key in clickUpgrades) {
+        let item = clickUpgrades[key]
+
+        tsec = (item.quantity * item.multiplier)
+
+    }
+
+}
 
 
 function buyWeights() {
@@ -55,28 +80,46 @@ function buyWeights() {
         testosterone -= clickUpgrades.weights.price
         clickUpgrades.weights.price *= 2
 
+        Tsec()
         draw()
 
     }
 }
 
 function buyGfuel() {
-    if (testosterone >= 40) {
+    if (testosterone >= clickUpgrades.gfuel.price) {
         clickUpgrades.gfuel.quantity += 1
         testosterone -= clickUpgrades.gfuel.price
+        clickUpgrades.gfuel.price *= 2
 
+        Tsec()
         draw()
 
     }
 }
 
 function buySupplements() {
-    if (testosterone >= 40) {
+    if (testosterone >= autoUpgrades.supplements.price) {
         autoUpgrades.supplements.quantity += 1
         testosterone -= autoUpgrades.supplements.price
+        autoUpgrades.supplements.price *= 2
 
 
+        Multiplier()
+        draw()
 
+    }
+
+}
+
+function buyCodeLessons() {
+    if (testosterone >= autoUpgrades.codelessons.price) {
+        autoUpgrades.codelessons.quantity += 1
+        testosterone -= autoUpgrades.codelessons.price
+        autoUpgrades.codelessons.price *= 2
+
+
+        Multiplier()
         draw()
 
     }
@@ -91,6 +134,23 @@ function draw() {
 
     document.getElementById("gfuel").innerText = "G-fuel : " + clickUpgrades.gfuel.quantity
 
+    document.getElementById("t-sec").innerText = "T/Sec : " + multiply
+
+    document.getElementById("multiplier").innerText = "T-Multiplier : " + tsec
+
+    drawButtons()
+
 }
 
+function drawButtons() {
+    document.getElementById("buy-weights").innerText = "Buy Weights: $" + clickUpgrades.weights.price
+
+    document.getElementById("buy-gfuel").innerText = "Buy G-fuel: $" + clickUpgrades.gfuel.price
+
+    document.getElementById("buy-supplements").innerText = "Buy Supplements: $" + autoUpgrades.supplements.price
+
+    document.getElementById("buy-code").innerText = "Buy Code Lessons: $" + autoUpgrades.codelessons.price
+}
+
+drawButtons()
 setInterval(addAuto, 3000);
